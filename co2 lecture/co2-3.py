85a0b2470126decdc99cfe0baad29a94c703a954
Nageshwari Bingi <2420030426@klh.edu.in>
2026-09-21 12:29:06 +0530

    Remove all files

--- co2 lecture/co2-3.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt 
-import pandas as pd
-df = pd.read_csv("Temporal.csv")
-x_values = [0,1,2,3,4,5]
-y_values=[0,1,4,9,16,25]
-plt.scatter(x_values,y_values,color='blue')
-plt.title('scatter plot')
-plt.show()
